cd37d0416d046c6322c52ec176a20c425a32809f
christopher dykstra <chrdyks@gmail.com>
2021-02-20 09:04:14 -0600

    Direct SQL implementation

---
+++ DockerComposeLocalStack/DataAccess/GetMessagesCommand.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+
+namespace DockerComposeLocalStack.DataAccess
+{
+    public class GetMessagesCommand : IGetMessagesCommand
+    {
+        private readonly IDbConnection _connection;
+
+        public GetMessagesCommand(IDbConnection connection)
+        {
+            _connection = connection;
+            _connection.Open();
+        }
+
+        public IEnumerable<string> GetMessages()
+        {
+            using var command = _connection.CreateCommand();
+
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT Value FROM Message;";
+
+            using var reader = command.ExecuteReader();
+            var valuePosition = reader.GetOrdinal("Value");
+
+            var messages = new Collection<string>();
+            while (reader.Read())
+            {
+                messages.Add(reader.GetString(valuePosition));
+            }
+
+            return messages;
+        }
+
+        public void Dispose() => _connection.Dispose();
+    }
+}
